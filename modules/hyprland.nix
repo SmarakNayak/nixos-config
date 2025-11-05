c3084ac110bd04fa90732dfadd77bd8b5cf13daf
Smarak Nayak <miltu.s.nayak@gmail.com>
2025-11-05 18:01:00 +1100

    autostart xdg on hyprland startup

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -14,11 +14,16 @@
       swaylock
     ];
 
-   wayland.windowManager.hyprland = {
-     enable = true;
-     systemd.enable = true;  # Creates hyprland-session.target
-     extraConfig = "# Using manual config file";  # Suppress warning
-   };
+    wayland.windowManager.hyprland = {
+      enable = true;
+      systemd.enable = true;  # Creates hyprland-session.target
+      extraConfig = "# Using manual config file";  # Suppress warning
+    };
+    
+    # Make hyprland-session start XDG autostart apps
+    systemd.user.targets.hyprland-session = {
+      Unit.Wants = [ "xdg-desktop-autostart.target" ];
+    };
 
     xdg.configFile."hypr/hyprland.conf".source = ../dotfiles/hypr/hyprland.conf;
     xdg.configFile."hypr/hyprpaper.conf".source = ../dotfiles/hypr/hyprpaper.conf;
@@ -47,5 +52,6 @@
         };
       };
     };
+
   };
 }
