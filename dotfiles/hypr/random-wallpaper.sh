0845ba1eed83335c3170ebd0f38f90ed8d584b43
Smarak Nayak <miltu.s.nayak@gmail.com>
2026-01-23 13:45:40 +1100

    update hypr config

--- dotfiles/hypr/random-wallpaper.sh
+++ dotfiles/hypr/random-wallpaper.sh
@@ -8,10 +8,7 @@ WALLPAPER_DIR="$HOME/nixos-config/wallpapers/catppuccin-mocha"
 # Pick a random wallpaper (excluding README.md and gifs)
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
 
-# Unload any previously loaded wallpapers to save memory
-hyprctl hyprpaper unload all
-
 # Set wallpaper on all monitors
 for monitor in $(hyprctl monitors -j | jq -r '.[].name'); do
-    hyprctl hyprpaper reload "$monitor,$WALLPAPER"
+    hyprctl hyprpaper wallpaper "$monitor,$WALLPAPER"
 done
