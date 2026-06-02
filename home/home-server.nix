b44137f283a53de941895d0fb15f3130b9643c48
Smarak Nayak <miltu.s.nayak@gmail.com>
2026-06-02 16:40:20 +1000

    removed hermes-sandbox in favour of the nous flake

--- home/home-server.nix
+++ home/home-server.nix
@@ -1,16 +1,11 @@
 { pkgs, ... }:
 
-let
-  hermes-sandbox = import ../packages/hermes-sandbox.nix { inherit pkgs; };
-in
-
 {
   imports = [
     ./core.nix
   ];
 
   home.packages = with pkgs; [
-    hermes-sandbox
     inxi
     pciutils
     hwinfo
